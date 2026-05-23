11f938d0a285c71865a58eefb20ef147bf11a250
Travis Wessman <traviswessman26@gmail.com>
2026-05-23 16:56:11 -0400

    completed birthday problem

--- 01-probability/probability.py
+++ 01-probability/probability.py
@@ -1,4 +1,18 @@
+# birthday problem
+n = 1
+for n in range(365):
+    product = 1
+    for i in range(1, n + 1):
+        product = product * (365 - i) / 365
+    prob = 1 - product
+    print(n)
+    print(prob)
+    if prob >= 0.5:
+        break
+
+
 # takes floats, returns posterior.
+
 def bayes(prior, likelihood, evidence):
     pass
 def cdf(x, distribution):
