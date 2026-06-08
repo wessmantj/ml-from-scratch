6ab0a0af081682cd5d7e6e94901321689e47b0d3
Travis Wessman <traviswessman26@gmail.com>
2026-06-08 16:24:03 -0400

    factorial function

--- 01-probability/probability.py
+++ 01-probability/probability.py
@@ -1,14 +1,81 @@
 # returns the probability that at least two of n people share a birthday.
 def birthday_problem(n):
     product = 1
-    for i in range(1, n + 1):
+    for i in range(n):
         product *= (365 - i) / 365
     return 1 - product
 
 # n = 22 is over 50%
 
+
+def factorial(n):
+    res = 1
+    
+    for i in range(1, n + 1):
+        res *= i
+    return res
+
+print(factorial(5))
+
+def n_choose_k(n, k):
+    pass
+
+def bernoulli_pmf(k, p):
+    pass
+
+def bernoulli_sample(k, n, p):
+    pass
+
+def binomial_pmf(k, n, p):
+    pass
+
+def gaussian_pdf(x, mu, sigma):
+    pass
+
+def gaussian_cdf(x, mu, sigma):
+    pass
+
+def multinomial_pmf(counts, probs):
+    pass
+
 def bayes(prior, likelihood, evidence): 
     pass
-def cdf(x, distribution):
+
+def bayes_posterior(priors, likelihood): 
     pass
+
+def expectations(values, probs):
+    pass
+
+def variance(values, probs):
+    pass
+
+def sample_mean(data):
+    pass
+
+def sample_variance(data, ddof):
+    pass
+
+def covariance(x, y):
+    pass
+
+def correlation(x, y):
+    pass
+
+def log_likelihood(data, dist, params):
+    pass
+
+def mle_bernoulli(data):
+    pass
+
+def mle_gaussian(data):
+    pass
+
+def simulate_lln(p, max_n):
+    pass
+
+def simulate_clt(sampler, num_samples, sample_size):
+    pass
+
+
 
